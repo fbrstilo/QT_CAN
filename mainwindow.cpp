0eb10191c7a55dc090cd8c0b123a996bb671c932
fbrstilo <fbrstilo@gmail.com>
2024-04-26 16:21:07 +0200

    Initial commit, finished basic GUI and message sending functionality

---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QCanBus>
+#include <QDebug>
+
+QCanBusDevice *device = nullptr;
+
+QCanBusDevice* DeviceInit(){
+    if(device) return device; //CAN device already connected
+
+    //device not connected
+    if (QCanBus::instance()->plugins().contains(QStringLiteral("socketcan"))) {
+        // plugin available
+        QString errorString;
+        QCanBusDevice *device = QCanBus::instance()->createDevice(
+            QStringLiteral("socketcan"), QStringLiteral("vcan0"), &errorString);
+        if (!device) {
+            qDebug() << errorString;
+        } else {
+            device->connectDevice();
+            return device;
+        }
+    }
+    return nullptr; // :(
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_sendMessage_clicked()
+{
+    if(device == nullptr){
+        device = DeviceInit();
+    }
+    QCanBusFrame frame;
+    frame.setFrameId(ui->messageID->text().toInt());
+    QByteArray payload(ui->messageData->text().toUtf8());
+    frame.setPayload(payload);
+    device->writeFrame(frame);
+}
+
+
